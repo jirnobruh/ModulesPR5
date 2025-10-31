31edbdc20c0a33d63ad364a99f64532fc9d9d2d5
Server\NOT/founD <purplefenix228@mail.ru>
2025-10-31 18:35:55 +0700

    End proj. New Applicants add

--- ModulesPR5/Helper.cs
+++ ModulesPR5/Helper.cs
@@ -20,23 +20,31 @@ namespace ModulesPR5
             return _context;
         }
         
-        public void CreateApplicants(Applicants applicant)
+        public static void CreateApplicants(Applicants applicant)
         {
             _context.Applicants.Add(applicant);
             _context.SaveChanges();
         }
         
-        public void UpdateApplicants(Applicants applicant)
+        public static void UpdateApplicants(Applicants applicant)
         {
             _context.Entry(applicant).State = System.Data.EntityState.Modified;
             _context.SaveChanges();
         }
         
-        public void RemoveApplicants(int idApplicant)
+        public static void RemoveApplicants(int idApplicant)
         {
             var users = _context.Applicants.Find(idApplicant);
             _context.Applicants.Remove(users);
             _context.SaveChanges();
+        }
+        
+        public static int CreateAuth(Auth auth)
+        {
+            _context.Auth.Add(auth);
+            _context.SaveChanges();
+            _context.Entry(auth).GetDatabaseValues();
+            return auth.id;
         }
     }
 }
--- ModulesPR5/Program.cs
+++ ModulesPR5/Program.cs
@@ -11,29 +11,55 @@ namespace ModulesPR5
     {
         public static void Main(string[] args)
         {
+            int status;
+            
             Console.WriteLine("Создание новой учетной записи для соискателя");
             
             Console.WriteLine();
             
             Console.Write("Введите имя пользователя: ");
             string lastName = Console.ReadLine();
-            Console.WriteLine();
             
             Console.Write("Введите фамилию пользователя: ");
             string firstName = Console.ReadLine();
-            Console.WriteLine();
             
             Console.Write("Введите отчество пользователя (при наличии): ");
             string middleName = Console.ReadLine();
-            Console.WriteLine();
+            
+            Console.Write("Введите электронную почту пользователя: ");
+            string email = Console.ReadLine();
+            
+            Console.Write("Введите номер телефона пользователя: ");
+            string phone = Console.ReadLine();
+            
+            Console.WriteLine("Введите цифру статуса пользователя пользователя: ");
+            Console.WriteLine("1 - безработный");
+            Console.WriteLine("2 - трудоустроен");
+            try
+            {
+                status = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка ввода: {ex.Message}");
+                Console.WriteLine("Введите цифру статуса пользователя пользователя: ");
+                Console.WriteLine("1 - безработный");
+                Console.WriteLine("2 - трудоустроен");
+                status = int.Parse(Console.ReadLine());
+            }
+            
+            Console.Write("Введите дату рождения (гггг-мм-дд): ");
+            DateTime birthDate;
+            while (!DateTime.TryParse(Console.ReadLine(), out birthDate))
+            {
+                Console.Write("Неверный формат даты. Введите дату рождения (гггг-мм-дд): ");
+            }
             
             Console.Write("Введите логин пользователя: ");
             string login = Console.ReadLine();
-            Console.WriteLine();
             
             Console.Write("Введите пароль пользователя: ");
             string password = Console.ReadLine();
-            Console.WriteLine();
 
             string hashPassw = HashPassword.GetHashPassword(password);
             
@@ -43,7 +69,32 @@ namespace ModulesPR5
             {
                 cadrAgEntities db = Helper.GetContext();
                 
+                Auth newAuth = new Auth
+                {
+                    login = login,
+                    password = hashPassw
+                };
                 
+                int authId = Helper.CreateAuth(newAuth);
+                Console.WriteLine($"Создана запись аутентификации с ID: {authId}");
+                
+                Applicants newApplicant = new Applicants
+                {
+                    last_name = lastName,
+                    first_name = firstName,
+                    middle_name = string.IsNullOrEmpty(middleName) ? null : middleName,
+                    birth_date = birthDate,
+                    email = email,
+                    phone = phone,
+                    status_id = status,
+                    auth_id = authId
+                };
+
+                // Добавляем в базу данных
+                Helper.CreateApplicants(newApplicant);
+                
+                Console.WriteLine("Соискатель успешно создан!");
+                Console.WriteLine($"ID нового соискателя: {newApplicant.id}");
             }
             catch (Exception e)
             {
